ea53a0c47a9367b42f80aef265e2504ad42c55d6
KhaledAsadw <163730959+KhaledAsadw@users.noreply.github.com>
2024-05-04 22:47:31 +0300

    Update channel.js

--- plugins/channel.js
+++ plugins/channel.js
@@ -1,7 +1,9 @@
 let handler = async (m, { conn }) => {
 
 m.reply(`*قناتي على الواتساب:*\n
-*https://whatsapp.com/channel/0029VaCoD2sAYlUSoRyroy1A*
+https://whatsapp.com/channel/0029VaCoD2sAYlUSoRyroy1A
+*كل ما هوا عميق🖤.*
+`•‏ مبتور قلبي، بحاجة إلى أعوام من السلام حتى يعود كما كان`🖤.
 *تابعني هناك♥*`)
 }
 handler.help = ['channel']
